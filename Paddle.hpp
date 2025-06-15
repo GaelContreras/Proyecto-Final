e5d0efe65af7e609eac834ce78f00bef0f4c6e72
Alex <a24110229@live.ceti.mx>
2025-06-15 16:22:31 +0000

    Paddle

---
+++ Paddle.hpp
@@ -0,0 +1,20 @@
+#ifndef PADDLE_HPP
+#define PADDLE_HPP
+
+#include <SFML/Graphics.hpp>
+
+class Paddle {
+public:
+    Paddle(float x, float y, float width, float height);
+    void move(float dx);
+    void update(float dt, const sf::RenderWindow& window);
+    void draw(sf::RenderWindow& window) const;
+    sf::FloatRect getBounds() const;
+    void reset(float x, float y);
+
+private:
+    sf::RectangleShape shape;
+    float speed;
+};
+
+#endif
