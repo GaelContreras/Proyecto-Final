01c850dcc3e8ac69a5bf64bcabacaf7f1ae97872
Alex <a24110229@live.ceti.mx>
2025-06-19 19:05:00 -0600

    Bricksc

--- Brick.cpp
+++ Brick.cpp
@@ -1,20 +1,22 @@
 #include "Brick.hpp"
 
+#include "Brick.hpp"
+
 Brick::Brick(float x, float y, float width, float height, sf::Color color)
-    :destroyed(false)
+    : destroyed(false)
 {
     shape.setPosition(x, y);
-    shape.setSize({width, height});
+    shape.setSize(sf::Vector2f(width, height));
     shape.setFillColor(color);
     shape.setOrigin(width / 2.0f, height / 2.0f);
 }
 
-void Brick::render(sf::Renderwindow& window) const{
+void Brick::draw(sf::RenderWindow& window) const {
     window.draw(shape);
 }
 
-sf::FloatRect Brick::getRoundRect() const {
-    return shape.getGlobal Bounds();
+sf::FloatRect Brick::getBounds() const {
+    return shape.getGlobalBounds();
 }
 
 bool Brick::isDestroyed() const {
