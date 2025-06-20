622882f6bd9121a9a3b3bb1e78846a1f2719c3d0
Alex <a24110229@live.ceti.mx>
2025-06-19 21:02:20 -0600

    Paddlecp

--- Paddle.cpp
+++ Paddle.cpp
@@ -4,21 +4,37 @@ Paddle::Paddle(float x, float y, float width, float height)
     : speed(400.0f)
 {
     shape.setPosition(x, y);
-    shape.setSize({width, height});
+    shape.setSize(sf::Vector2f(width, height));
     shape.setFillColor(sf::Color::White);
-    shape.setOrigin(width / 0.2f, height / 0.2f);
+    shape.setOrigin(width / 2.0f, height / 2.0f);
 }
 
 void Paddle::move(float dx) {
     shape.move(dx, 0);
 }
 
-void Paddle::update(float dt, const sf::RenderWindow& Window) {
+void Paddle::update(float dt, const sf::RenderWindow& window) {
+    float dx = 0.0f;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        dx -= speed * dt;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        dx += speed * dt;
 
+  
+    sf::Vector2f pos = shape.getPosition();
+    float halfWidth = shape.getSize().x / 2.0f;
+    float nextX = pos.x + dx;
+    if (nextX - halfWidth < 0) {
+        dx = 0 - (pos.x - halfWidth);
+    } else if (nextX + halfWidth > window.getSize().x) {
+        dx = window.getSize().x - (pos.x + halfWidth);
+    }
+
+    move(dx);
 }
 
-void Paddle::draw(sf::RenderWindow& Window) const{
-    window.draw(shape);
+void Paddle::draw(sf::RenderWindow& window) const {
+    window.draw(shape); 
 }
 
 sf::FloatRect Paddle::getBounds() const {
